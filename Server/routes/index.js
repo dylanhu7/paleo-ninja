e2f111e272fbbffe0c8821d67c1d85151e9b0f0b
Dylan Hu <dylanhu7@gmail.com>
2020-11-28 23:39:09 -0500

    store pro, wins, dino, eatingHabits in database

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -6,32 +6,38 @@ const axios = require("axios");
 const mysql = require("mysql");
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'toor',
-  database: 'paleoninja',
+  host: "localhost",
+  user: "root",
+  password: "toor",
+  database: "paleoninja",
   multipleStatements: true
-})
+});
 
 router.get("/", (req, res) => {
-  pool.getConnection(function (err, connection) {
+  pool.getConnection(function(err, connection) {
     if (err) throw err;
-      limit = 5;
-      connection.query({
-              sql: 'SELECT * FROM `pro-dino` ORDER BY RAND() LIMIT ?',
-              values: [limit],
-          }, function (err, result) {
-              if (err) {
-                  console.error(err)
-                  res.send('An error has occurred')
-                  return
-              }
-              console.log("result: " + result[0]['resText'])
-              res.render('index', {e1: result[0]['resText'], e2: result[1]['resText'], e3: result[0]['resText'], e4: result[0]['resText'], e5: result[0]['resText']});
-          }
-      )
+    limit = 5;
+    connection.query(
+      {
+        sql: "SELECT * FROM `pro-dino` ORDER BY RAND() LIMIT ?",
+        values: [limit]
+      },
+      function(err, result) {
+        if (err) {
+          console.error(err);
+          res.send("An error has occurred");
+          return;
+        }
+        res.render("index", {
+          e0: result[0] ? result[0] : "",
+          e1: result[1] ? result[1] : "",
+          e2: result[2] ? result[2] : "",
+          e3: result[3] ? result[3] : "",
+          e4: result[4] ? result[4] : ""
+        });
+      }
+    );
   });
-  //res.render("index");
 });
 
 module.exports = router;
